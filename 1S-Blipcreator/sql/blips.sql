5da6fd70acdfaa6666c101d2e8ada02daf909b61
1S <155136643+AcetheBreez@users.noreply.github.com>
2024-10-18 20:58:00 +1100

    Delete 1S-Blipcreator directory

--- 1S-Blipcreator/sql/blips.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `blips` (
-    `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `name` VARCHAR(255) NOT NULL,
-    `sprite` INT NOT NULL,
-    `color` INT NOT NULL,
-    `x` FLOAT NOT NULL,
-    `y` FLOAT NOT NULL,
-    `z` FLOAT NOT NULL
-);
